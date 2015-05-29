c530c22d4c4bdbaa383c5ad61845eb37dc75e60b
Jxck <block.rxckin.beats@gmail.com>
2015-05-29 12:28:22 +0900

    add name rules

--- tests/rules/name.js
+++ tests/rules/name.js
@@ -7,30 +7,49 @@ var eslintTester = new ESLintTester(linter);
 
 var valid = [
   'class Foo {' +
+  '}          ',
+
+  'class Foo {' +
+  '  bar() {} ' +
   '}          '
 ].map(function(code) {
   return {
     code: code,
-    args: [ 1, 'class' ],
+    args: [ 1, 'class', 'method' ],
     ecmaFeatures: { classes: true }
   };
 });
 
 var klass = 'class name should start with upper case.';
 
-var invalid = [
+var invalidClass = [
   'class foo {' +
   '}          '
 ].map(function(code) {
   return {
     code: code,
-    args: [ 1, 'class' ],
+    args: [ 1, 'class', 'method' ],
     ecmaFeatures: { classes: true },
     errors: [{ message: klass }]
   };
 });
 
+var method = 'method name should start with lower case.';
+
+var invalidMethod = [
+  'class Foo {' +
+  '  Bar(){}  ' +
+  '}          '
+].map(function(code) {
+  return {
+    code: code,
+    args: [ 1, 'class', 'method' ],
+    ecmaFeatures: { classes: true },
+    errors: [{ message: method }]
+  };
+});
+
 eslintTester.addRuleTest('./lib/rules/name', {
   valid: valid,
-  invalid: invalid
+  invalid: invalidClass.concat(invalidMethod)
 });
